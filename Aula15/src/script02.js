4a61514e2338b16e900a3e904754c84f068d98dc
keylla cristina <keyllac558@gmail.com>
2026-03-19 20:17:45 -0300

    Aula 16 - Alterando o Estilo de Elementos com o DOM

---
+++ Aula15/src/script02.js
@@ -0,0 +1,16 @@
+// Utilização o getElementById()
+const elemento1 = document.getElementeById ("selecionandoElemento");
+
+console.log(elemento1);
+
+// Utilizando o getElementsByTagName()
+const elemento2 = document.getElementsByTagName("h1");
+
+console.log(elemento2[0]);
+
+// Utilizando o getElementosByClassName()
+const elemento3 = document.getElementsByClassName ("selecionando ElementoporClasse ");
+
+console.log(elemento3[0]);
+
+
